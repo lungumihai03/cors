3a16d113a50d7882e2bd8939d9a1627f6bafc62a
Mihai Lungu <lungu.mihai03@gmail.com>
2024-12-23 23:29:27 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const corsAnywhere = require('cors-anywhere');
 const host = '0.0.0.0'; // Ascultă pe toate interfețele
 const port = process.env.PORT || 8080;
 
+// Creează serverul
 corsAnywhere.createServer({
-    originWhitelist: [], // Permite toate origin-urile
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2'],
+    originWhitelist: [], // Permite toate origin-urile (fără restricții)
+    requireHeader: [], // Elimină cerința pentru anteturi (ex. origin, x-requested-with)
+    removeHeaders: ['cookie', 'cookie2'], // Opțional: elimină anteturile sensibile
 }).listen(port, host, () => {
     console.log(`CORS Proxy server is running on ${host}:${port}`);
 });
